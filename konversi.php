0f28f11289b8be9bc830184af1198204691d45e6
Icha Pratilla <50561943+ichapratilla@users.noreply.github.com>
2022-10-14 16:14:00 +0700

    Add files via upload
    konversi mata uang

---
+++ konversi.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<!-- <link type="text/css" rel="stylesheet" href="currency_style.css"/> -->
+</head>
+<body>
+<div id="wrapper">
+
+<div id="convert_div">
+<form method="post"action="convert_currency.php">
+ <input type="text" name="amount" placeholder="Enter Amount">
+ <select name="convert_from">
+  <option  value="IDR">Indonesia</option>
+ </select>
+ <select name="convert_to">
+  <option  value="USD">Bitcoint</option>
+  
+ </select>
+ <br>
+ <input type="submit" name="convert_currency" value="Convert Currency">
+</form>
+</div>
+
+</div>
+</body>
+</html>
